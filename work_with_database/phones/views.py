17e5173aee0dfdb879f9b9a94f7ef5feaf61b96e
Bondarev Artem <bond210484@mail.ru>
2024-09-22 11:35:08 +0700

    add func sort

--- work_with_database/phones/views.py
+++ work_with_database/phones/views.py
@@ -11,19 +11,41 @@ def show_catalog(request):
     template = 'catalog.html'
     phone_data = Phone.objects.all()
 
+    # Получить текущий параметр сортировки
+    sort_param = request.GET.get('sort')
+    if not sort_param:
+        sort_param = 'name'
+
+    # Применить фильтр к данным
+    if sort_param == 'name':
+        phone_data = phone_data.order_by('name')
+    elif sort_param == 'min_price':
+        phone_data = phone_data.order_by('price')
+    else: #sort_param == 'max_price'
+        phone_data = phone_data.order_by('-price')
+
     context = {
-        'phones' : phone_data
+        'phones': phone_data,
+        'sort_by': sort_param
     }
     return render(request, template, context)
+# def show_catalog(request):
+#     template = 'catalog.html'
+#     phone_data = Phone.objects.all()
+#
+#     context = {
+#         'phones' : phone_data
+#     }
+#     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    phone_data = Phone.objects.all()
-    for phone in phone_data:
+    phone_data = Phone.objects.get(slug=slug)
 
-        context = {
-            'phone': phone
+
+    context = {
+        'phone': phone_data
         }
     return render(request, template, context)
 
